3acc97d21d26a19fc4bae71ce404dc1bf6c46953
Cinnolia <cinnnolia@outlook.com>
2025-04-18 10:41:55 -0500

    added a cloud that can now move up and down!

--- Game.py
+++ Game.py
@@ -11,15 +11,34 @@ class Game:
 
         self.clock = pygame.time.Clock() 
 
-        self.image.load('')
+        self.img = pygame.image.load('assets\images\clouds\cloud_2.png')
 
-def run(self):
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()    
+        self.img_pos = [160,260]
+        self.movement = [False, False]
     
-        pygame.display.update()
-        self.clock.tick(60)
+    def run(self):
+        while True:
+            self.window.fill((0,0,0))
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.window.blit(self.img, self.img_pos)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()   
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+                 
+        
+            pygame.display.update()
+            self.clock.tick(60)
 Game().run()
+#picture this: eating a burger on the job, boss or customer walks inshe literally puts whats left of it in her pocket and when they leave she just like... yoinks it out and continues eating it... (lol me core)
