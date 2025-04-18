67374f852c3d18bbc5640383759b7419ff80e496
Cinnolia <cinnnolia@outlook.com>
2025-04-18 12:17:19 -0500

    working collision detection test

--- Game.py
+++ Game.py
@@ -12,13 +12,23 @@ class Game:
         self.clock = pygame.time.Clock() 
 
         self.img = pygame.image.load('assets\images\clouds\cloud_2.png')
-
+        self.img.set_colorkey((0,0,0))
         self.img_pos = [160,260]
         self.movement = [False, False]
+
+        self.collisoion_area = pygame.Rect(50, 50, 300, 50)
     
     def run(self):
         while True:
-            self.window.fill((0,0,0))
+            self.window.fill((0,56,0))
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            
+            if img_r.colliderect(self.collisoion_area):
+                pygame.draw.rect(self.window, (0,100,225),self.collisoion_area)
+            else:
+                pygame.draw.rect(self.window, (0,100,125),self.collisoion_area)
+            
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.window.blit(self.img, self.img_pos)
 
