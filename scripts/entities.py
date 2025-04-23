a22be0735837dada900057947e613cea33179f10
cardamon-admin <cinnolia@gmail.com>
2025-04-23 10:56:24 -0500

    the player can now jump infinitely but its a start

--- scripts/entities.py
+++ scripts/entities.py
@@ -7,6 +7,7 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
+        self.collisions = {'up' : False, 'down': False, 'left': False, 'right': False}
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
@@ -14,15 +15,16 @@ class PhysicsEntity:
     def update(self, tilemap, movement = (0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
-
         self.pos[0] += frame_movement[0]
         entity_rect = self.rect()
         for rect in tilemap.physics_rects_around(self.pos):
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
                     entity_rect.right = rect.left
+                    self.collisions['right'] = True
                 if frame_movement[0] < 0:
                     entity_rect.left = rect.right
+                    self.collisions['left'] = True
                 self.pos[0] = entity_rect.x
     
         self.pos[1] += frame_movement[1]
@@ -31,12 +33,18 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
                     entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
                     self.velocity[1] = 0
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
                     self.velocity[1] = 0
                 self.pos[1] = entity_rect.y
+
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+       # if self.collisions['down'] or self.collisions['up']:
+       #     self.velocity[1] = 0
 
     def render(self, surf):
         surf.blit(self.game.assets['player'], self.pos)
