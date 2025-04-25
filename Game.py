48b2c4322699b8aede5282e5d8670255230814e9
cardamon-admin <cinnolia@gmail.com>
2025-04-25 10:39:42 -0500

    working camera

--- Game.py
+++ Game.py
@@ -24,7 +24,8 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png')
         }
         print(self.assets)
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
@@ -36,12 +37,16 @@ class Game:
 
     def run(self):
         while True:
-            self.display.fill((0,28,0))
+            self.display.blit(self.assets['background'], (0, 0))
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
             
-            self.tilemap.render(self.display, offset = self.scroll)
+            self.tilemap.render(self.display, offset = render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0] , 0))
-            self.player.render(self.display, offset = self.scroll)
+            self.player.render(self.display, offset = render_scroll)
 
 
             
