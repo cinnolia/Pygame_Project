d21a73ba5f4f24f1b9b8791a7189586b18350541
cardamon-admin <cinnolia@gmail.com>
2025-04-30 09:51:49 -0500

    animation class

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,13 @@ def load_images(path):
     for img_name in os.listdir(BASE_IMG_PATH + path):
         images.append(load_image(path + '/' + img_name))
     return images
+
+class Animation:
+    def __init__(self, images, frame_rate = 10, loop = True):
+        self.images = images
+        self.frame_rate = frame_rate
+        self.loop = loop
+        self.current_frame = 0
+        self.done = False
+
+    
