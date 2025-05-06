e2eea14431651f348993ae64086970bae16f71f5
Cinnolia <cinnnolia@outlook.com>
2025-05-05 21:16:19 -0500

    animations

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,24 @@ def load_images(path):
     for img_name in os.listdir(BASE_IMG_PATH + path):
         images.append(load_image(path + '/' + img_name))
     return images
+
+#animation class
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True):
+        self.images = images #self.images gets all the images available essentialy not necessarily the ones neede for that specific animation which is why in def copy() we make another instance that doesnt load a bunch of images that we dont need
+        self.loop = loop
+        self.img_duration = img_dur 
+        self.done = False 
+        self.frame = 0
+
+    def copy(self):
+        return Animation(self.images, self.loop, self.img_duration)
+    
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images))
+    
+    def img(self):
+        return self.images [int(self.frame / self.img_duration)]
