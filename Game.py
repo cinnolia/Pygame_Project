db6454298baa67a80218072f66a8eeba60ad67df
Cinnolia <cinnnolia@outlook.com>
2025-06-03 00:17:17 -0500

    Add spark effect for projectiles and enemy actions

--- Game.py
+++ Game.py
@@ -7,6 +7,7 @@ from scripts.utils import load_image, load_images, Animation
 from scripts.entities import PhysicsEntity, Player, Enemy
 from scripts.tilemap import Tilemap
 from scripts.particle import Particle
+from scripts.sparks import Spark
  
 class Game:
     def __init__(self):
@@ -55,6 +56,7 @@ class Game:
         
         self.projectiles = [] # list of projectiles, not used yet
         self.particles = []
+        self.sparks = []
 
         self.scroll = [0,0] 
     
@@ -105,12 +107,25 @@ class Game:
                 self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1]))
                 if self.tilemap.solid_check(projectile[0]):
                     self.projectiles.remove(projectile)
+                    for i in range(4):
+                            self.sparks.append(Spark(projectile[0],random.random() -0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random()))
                 elif projectile[2] > 360:
                     self.projectiles.remove(projectile)
                 elif abs(self.player.dashing) > 50:
                     if self.player.rect().collidepoint(projectile[0]):
                         self.projectiles.remove(projectile)
-
+                        for i in range(30):
+                            angle = random.random() * math.pi * 2
+                            speed = random.random() * 5
+                            self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random()))
+                            self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+                        
+            for spark in self.sparks.copy():
+                kill = spark.update()
+                spark.render(self.display, offset=render_scroll)
+                if kill:
+                    self.sparks.remove(spark)
+                    
             for particle in self.particles.copy():
                 kill = particle.update()
                 particle.render(self.display, offset=render_scroll)
--- scripts/entities.py
+++ scripts/entities.py
@@ -4,6 +4,8 @@ import random
 import pygame
 
 from scripts.particle import Particle
+from scripts.sparks import Spark
+
 
 class PhysicsEntity:
     def __init__(self, game, e_type, pos, size):
@@ -95,8 +97,12 @@ class Enemy(PhysicsEntity):
                 if (abs(dis[1]) < 16):
                     if (self.flip and dis[0] < 0):
                         self.game.projectiles.append([[self.rect().centerx -7, self.rect().centery], -1.5, 0])
+                        for i in range(4):
+                            self.game.sparks.append(Spark(self.game.projectiles[-1][0],random.random() -0.5 + math.pi, 2 + random.random()))
                     elif (not self.flip and dis[0] > 0):
                         self.game.projectiles.append([[self.rect().centerx + 7, self.rect().centery], 1.5, 0])
+                        for i in range(4):
+                            self.game.sparks.append(Spark(self.game.projectiles[-1][0],random.random() -0.5 + math.pi, 2 + random.random()))
         elif random.random() < 0.01:
             self.walking = random.randint(30, 120)
 
