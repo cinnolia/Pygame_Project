2c79d0416b27c594fa25e24e4b9437cae3f71ce0
Cinnolia <cinnnolia@outlook.com>
2025-06-03 00:18:32 -0500

    Fix spark rendering color and adjust position calculations

--- scripts/sparks.py
+++ scripts/sparks.py
@@ -7,20 +7,20 @@ class Spark:
         self.pos = list(pos)
         self.angle = angle
         self.speed = speed
-
+        
     def update(self):
         self.pos[0] += math.cos(self.angle) * self.speed
         self.pos[1] += math.sin(self.angle) * self.speed
-
+        
         self.speed = max(0, self.speed - 0.1)
-        return not self.speed  # Return True if the spark should be removed
+        return not self.speed
     
     def render(self, surf, offset=(0, 0)):
         render_points = [
-            (self.pos[0] + math.cos(self.angle) * self.speed *3 + offset[0],self.pos[1] + math.sin(self.angle) * self.speed * 3 + offset[1]),
-            (self.pos[0] + math.cos(self.angle + math.pi * 0.5) * self.speed *3 + offset[0],self.pos[1] + math.sin(self.angle + math.pi * 0.5) * self.speed * 3 + offset[1]),
-            (self.pos[0] + math.cos(self.angle + math.pi) * self.speed *3 + offset[0],self.pos[1] + math.sin(self.angle + math.pi) * self.speed * 3 + offset[1]),
-            (self.pos[0] + math.cos(self.angle - math.pi * 0.5) * self.speed *3 + offset[0],self.pos[1] + math.sin(self.angle - math.pi * 0.5) * self.speed * 3 + offset[1]),
-        ]  
-
-        pygame.draw.polygon(surf, (255, 255, 0), render_points)
+            (self.pos[0] + math.cos(self.angle) * self.speed * 3 - offset[0], self.pos[1] + math.sin(self.angle) * self.speed * 3 - offset[1]),
+            (self.pos[0] + math.cos(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[0], self.pos[1] + math.sin(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[1]),
+            (self.pos[0] + math.cos(self.angle + math.pi) * self.speed * 3 - offset[0], self.pos[1] + math.sin(self.angle + math.pi) * self.speed * 3 - offset[1]),
+            (self.pos[0] + math.cos(self.angle - math.pi * 0.5) * self.speed * 0.5 - offset[0], self.pos[1] + math.sin(self.angle - math.pi * 0.5) * self.speed * 0.5 - offset[1]),
+        ]
+        
+        pygame.draw.polygon(surf, (255, 255, 255), render_points)
